d909050a8cc8573ac66c8c1dcfbe1f7f43c91795
Angie <liya0706s@gmail.com>
2024-06-10 14:24:47 +0800

    use route parameters passing data

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;  //確保引入控制器
 
-Route::get('/hihi', function () {
-    return view('welcome');
-});
+// Route::get('/hihi', function () {
+//     return view('welcome');
+// });
 
-Route::get('admin/posts/example', array('as'=>'admin.home' ,function () {
+// Route::get('admin/posts/example', array('as'=>'admin.home' ,function () {
 
-    $url = route('admin.home');
+//     $url = route('admin.home');
 
-    return "this url is ". $url;
+//     return "this url is ". $url;
 
-}));
+// }));
+
+Route::get('/post/{id}', [PostsController::class, 'index']);  //使用控制器類和方法的數組形成
